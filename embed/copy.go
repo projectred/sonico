64a1527805aa745355a8b190e3b385986088f376
veiforever <veiforever@outlook.com>
2022-10-04 00:21:44 +0800

    add method CoverCopy and AppendCopy

--- embed/copy.go
+++ embed/copy.go
@@ -6,7 +6,15 @@ import (
 	"path"
 )
 
-func Copy(fs embed.FS, dir, targetPath string, cover bool) error {
+func CoverCopy(fs embed.FS, dir, targetPath string) error {
+	return Copy(fs, dir, targetPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC)
+}
+
+func AppendCopy(fs embed.FS, dir, targetPath string) error {
+	return Copy(fs, dir, targetPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND)
+}
+
+func Copy(fs embed.FS, dir, targetPath string, flag int) error {
 	files, err := fs.ReadDir(dir)
 	if err != nil {
 		return err
@@ -14,12 +22,6 @@ func Copy(fs embed.FS, dir, targetPath string, cover bool) error {
 	if err := os.MkdirAll(targetPath, 0754); err != nil {
 		return err
 	}
-	flag := func() int {
-		if cover {
-			return os.O_CREATE | os.O_WRONLY
-		}
-		return os.O_EXCL | os.O_WRONLY
-	}()
 	for _, file := range files {
 		datas, err := fs.ReadFile(path.Join(dir, file.Name()))
 		if err != nil {
@@ -27,14 +29,11 @@ func Copy(fs embed.FS, dir, targetPath string, cover bool) error {
 		}
 
 		if err := func() error {
-			writer, err := os.OpenFile(path.Join(targetPath, file.Name()), flag, 0666)
-			if err == os.ErrExist && !cover {
-				return nil
-			}
+			writer, err := os.OpenFile(path.Join(targetPath, file.Name()), flag, 0644)
+			defer writer.Close()
 			if err != nil {
 				return err
 			}
-			defer writer.Close()
 			if _, err := writer.Write(datas); err != nil {
 				return err
 			}
--- embed/copy_test.go
+++ embed/copy_test.go
@@ -13,7 +13,7 @@ var fs embed.FS
 
 func TestCopy(t *testing.T) {
 	targetDir := path.Join(t.TempDir(), "config")
-	if err := Copy(fs, "test", targetDir, true); err != nil {
+	if err := Copy(fs, "test", targetDir, os.O_CREATE|os.O_WRONLY); err != nil {
 		t.Fatal(err)
 	}
 	files, err := os.ReadDir(targetDir)
@@ -39,5 +39,79 @@ func TestCopy(t *testing.T) {
 	}
 	if outPut.Work != "ok" {
 		t.Fatalf("it should be 'ok', but it is %s", outPut.Work)
+	}
+}
+
+func TestConveyCopy(t *testing.T) {
+	targetDir := path.Join(t.TempDir(), "config")
+	if err := Copy(fs, "test", targetDir, os.O_CREATE|os.O_WRONLY); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.WriteFile(path.Join(targetDir, "test_data.json"), []byte("just a test"), 0666); err != nil {
+		t.Fatal(err)
+	}
+	if err := CoverCopy(fs, "test", targetDir); err != nil {
+		t.Fatal(err)
+	}
+	files, err := os.ReadDir(targetDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(files) != 1 {
+		t.Fatalf("is should has len 1, but it is %d", len(files))
+	}
+	if files[0].Name() != "test_data.json" {
+		t.Fatalf("it's should be 'test_data.json', but it is %s", files[0].Name())
+	}
+	datas, err := os.ReadFile(path.Join(targetDir, files[0].Name()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	outPut := struct {
+		Work string `json:"work"`
+	}{}
+	if err := json.Unmarshal(datas, &outPut); err != nil {
+		t.Fatal(err)
+	}
+	if outPut.Work != "ok" {
+		t.Fatalf("it should be 'ok', but it is %s", outPut.Work)
+	}
+}
+
+func TestAppendCopy(t *testing.T) {
+	targetDir := path.Join(t.TempDir(), "config")
+	if err := Copy(fs, "test", targetDir, os.O_CREATE|os.O_WRONLY); err != nil {
+		t.Fatal(err)
+	}
+	if err := AppendCopy(fs, "test", targetDir); err != nil {
+		t.Fatal(err)
+	}
+	files, err := os.ReadDir(targetDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(files) != 1 {
+		t.Fatalf("is should has len 1, but it is %d", len(files))
+	}
+	if files[0].Name() != "test_data.json" {
+		t.Fatalf("it's should be 'test_data.json', but it is %s", files[0].Name())
+	}
+	datas, err := os.ReadFile(path.Join(targetDir, files[0].Name()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	outPut := struct {
+		Work string `json:"work"`
+	}{}
+	for start, end := 0, len(datas)/2; end == len(datas); start, end = end, len(datas) {
+		if err := json.Unmarshal(datas[start:end], &outPut); err != nil {
+			t.Fatal(err)
+		}
+		if outPut.Work != "ok" {
+			t.Fatalf("it should be 'ok', but it is %s", outPut.Work)
+		}
+		outPut.Work = ""
 	}
 }
